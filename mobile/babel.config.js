b004ad8825f3b6f5663cbfeb5c60fe5f65d9986f
Prateek <prateekupadhyay162002@gmail.com>
2026-03-06 22:41:35 +0530

    fix: add Babel class-properties plugin for true Web model compilation

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -4,6 +4,7 @@ module.exports = function (api) {
         presets: ['babel-preset-expo'],
         plugins: [
             ['@babel/plugin-proposal-decorators', { legacy: true }],
+            ['@babel/plugin-transform-class-properties', { loose: true }],
             'react-native-reanimated/plugin',
         ],
     };
